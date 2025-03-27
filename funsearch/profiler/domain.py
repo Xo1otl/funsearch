3145e8af163ee4ac790129c50d68426d6e623b59
Xo1otl <xolotl.mictl4n@gmail.com>
2025-03-27 14:50:31 +0000

    profilerのclean architecture

---
+++ funsearch/profiler/domain.py
@@ -0,0 +1,17 @@
+from typing import Callable, Protocol, Any
+
+type Remove = Callable[[], None]
+
+
+class Event(Protocol):
+    @property
+    def type(self) -> str: ...
+
+    @property
+    def payload(self) -> Any: ...
+
+
+# 決定的でない呼び出しが多いコンポーネントで、http サーバーのように profiler を刺してイベントのプロファイリングを行う設計をするためのプロトコル
+class Pluggable[T: Event](Protocol):
+    def use_profiler(self, profiler_fn: Callable[[T], None]) -> Remove:
+        ...
